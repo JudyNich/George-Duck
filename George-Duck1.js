74f9e37760a32044574b4617f60759e765e5e473
Mary Reese <31937314+mreese23@users.noreply.github.com>
2017-09-28 16:10:49 -0400

    Color Fix
    "rainColor = random(0,250);" hadn't actually been doing anything. I'm not exactly sure why that is, but generating random numbers and repeating the fill within the if function works. It is changing the values of the variables and then applying the fill function using those new values every time it fulfills the pre reqs for the function.

--- George-Duck1.js
+++ George-Duck1.js
@@ -1,26 +1,31 @@
 var xPositions = [50, 100, 200, 250, 300];
 var yPositions = [0, 10, 15, 20, 25];
-var firstColor = random(50,150);  //fill with random colors 1st place (low to high)
+var firstColor = random(50,150);   
 var secColor = random(0, 250);
-var thirdColor = random(100, 200);   //fill with random color 2nd place
+var thirdColor = random(100, 200);   
 var rainColor = fill(firstColor, secColor, thirdColor);
 
 
 draw = function() {
     background(204, 247, 255);
-    rainColor = random(0,250);
+    
 
     for (var i = 0; i < xPositions.length; i++) {
         noStroke();
         ellipse(xPositions[i], yPositions[i], 5, 5);
+        
 
         if (yPositions[i] > 400) {
             yPositions[i]=0;
+             firstColor = random(50,150);   
+             secColor = random(0, 250);
+             thirdColor = random(100, 200);   
+             rainColor = fill(firstColor, secColor, thirdColor);
+        
 
         }
-
-
-
+        
+       
         {
             yPositions[i] += 5;
 
